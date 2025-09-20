cc9f3a78de53477b0b48e415f284b331abf55707
muradhani <mouradhany574@gmail.com>
2025-09-20 21:53:08 +0300

    Refactor MainActivity by removing unused imports

--- app/src/main/java/com/devcraft/tiktok/MainActivity.kt
+++ app/src/main/java/com/devcraft/tiktok/MainActivity.kt
@@ -5,25 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
-import android.net.Uri
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import com.devcraft.tiktok.features.screen.EditorScreen
-import com.devcraft.tiktok.features.screen.HomeScreen
-import com.devcraft.tiktok.features.screen.SettingsScreen
 import com.example.videoeditor.ui.theme.VideoEditorTheme
 
 class MainActivity : ComponentActivity() {
